6e75879d4472de76fb98a618c18f36857aeb375e
nurmuh <nurmuhamadrum@gmail.com>
2025-06-07 15:15:45 +0700

    create middleware

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['jwt.verify'])->get('test',function () {
+    return 'success';
+});
