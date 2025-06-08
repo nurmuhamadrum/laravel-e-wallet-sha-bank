8babffdb926e5b36201408d65fd972f03c97b82a
nurmuh <nurmuhamadrum@gmail.com>
2025-06-08 15:39:04 +0700

    create payment midtrans redirect finish

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedirectPaymentController;
+use Illuminate\Support\Facades\Redirect;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/payment-finish', function () {
+    return view('payment-finish', [RedirectPaymentController::class, 'finish']);
 });
